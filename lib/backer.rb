2e5089c05eca765e0ac293db03849a46d0d7d846
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-25 22:07:54 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+  attr_accessor :name, :backed_projects
+  
+  @@all = []
+  
+  def initialize(name,backed_projects = [])
+    @name = name
+    @backed_projects = backed_projects
+    @@all << self
+  end
+  
+  def back_project(project)
+  @backed_projects << project
+  project.add_backer(self)
+  end
+  
+  
+  
+  
+end
